f0d6c681eec1818fc09045c225da8047852b3f68
dseymo02 <dseymo02@dcs.bbk.ac.uk>
2018-05-29 11:54:05 +0100

    more

--- question6.py
+++ question6.py
@@ -1,4 +1,19 @@
-nums = [1,2,3,4,5,6]
+#a.
+
+nums = [1,2,-3,4,5,6]
 temp = nums[0] + nums[-1]
 nums.append(nums[0] + nums[-1])
 print(nums)
+
+#b.
+
+for i in nums:
+	if i < 0:
+		nums.remove(i)
+print(nums)
+
+#c.
+a = 3
+b = 4
+c = 3
+print("The average is " + str((a+b+c)/3))
