f3d5e80f21c8c06125a62c3401c3ea4c0ebc9a95
dseymo02 <dseymo02@dcs.bbk.ac.uk>
2018-05-29 11:15:15 +0100

    more

--- question2a.py
+++ question2a.py
@@ -1,2 +1,10 @@
+import pytest
+
 def search(s,slst,size):
-	pass
+	result = -1
+	for index in range(0,size):
+		if s == slst[index]:
+			result = index
+	return result
+
+print(search("hell",["hi","hello"],2)) 
